fb3b99cbfddbcfb422a9dde59502c0ddfb7e3fea
habulCoderJohnny <fokruljohnny@gmail.com>
2023-07-27 22:36:30 +0600

    Firebase config changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQXlpBc6JWTf8n4oBV9uftWScI2FzYHwM",
-  authDomain: "common-project-000.firebaseapp.com",
-  databaseURL: "https://common-project-000-default-rtdb.firebaseio.com",
-  projectId: "common-project-000",
-  storageBucket: "common-project-000.appspot.com",
-  messagingSenderId: "789278346215",
-  appId: "1:789278346215:web:2d8dca718495177bdd8b42",
+  apiKey: "AIzaSyBDj5rnzylFza8SyMcsXJg5iQfNtVqInww",
+  authDomain: "boinamadotcom.firebaseapp.com",
+  projectId: "boinamadotcom",
+  storageBucket: "boinamadotcom.appspot.com",
+  messagingSenderId: "345388133135",
+  appId: "1:345388133135:web:420e01c4cb21ee0f042c7a",
 };
 
 const app = initializeApp(firebaseConfig);
